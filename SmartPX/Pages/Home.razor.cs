98d8acad03a572acba19980b46345642e11a9c4c
RubenPX <rubenmarchante@gmail.com>
2024-12-26 02:15:46 +0100

    Blazor: Add api & test

---
+++ SmartPX/Pages/Home.razor.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace SmartPX.Pages;
+
+public partial class Home : ComponentBase {
+    [Inject] private SmartPXApi api { get; set; } = null!;
+    
+    public void runTestApi() {
+        api.GetDeviceLogs("bf7c446508d30254c9xv8k");
+    }
+}
---
+++ SmartPX/Services/SmartPXApi.cs
@@ -0,0 +1,13 @@
+﻿namespace SmartPX;
+
+public class SmartPXApi {
+    private readonly HttpClient client = new HttpClient() { BaseAddress = new Uri("http://localhost:5062") };
+
+    public async Task helloRoute() {
+        client.GetAsync("/tuya");
+    }
+    
+    public async Task GetDeviceLogs(string deviceId, string type = "1,2,3,4,5,6,7,8,9,10", long startTime = 0L, long? endTime = null) {
+        client.GetAsync($"/tuya/device/{deviceId}/logs?type={type}&start_time={startTime}&end_time={endTime}");
+    }
+}
